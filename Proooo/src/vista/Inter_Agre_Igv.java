1ff23a1ddd18146ea75c49270772500128cd155d
fredy <fredysunajma@gmail.com>
2019-02-13 09:54:14 -0500

    se agrego vistas de Marca y igv actualizado

--- Proooo/src/vista/Inter_Agre_Igv.java
+++ Proooo/src/vista/Inter_Agre_Igv.java
@@ -33,7 +33,10 @@ public class Inter_Agre_Igv extends javax.swing.JInternalFrame {
         jButton1 = new javax.swing.JButton();
         txt_codigo_igv = new javax.swing.JTextField();
         txt_monto_igv = new javax.swing.JTextField();
-        txt_descripcion_igv = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txt_descripcion_igv = new javax.swing.JTextArea();
+        jLabel4 = new javax.swing.JLabel();
+        txt_anio_igv = new javax.swing.JTextField();
 
         setClosable(true);
         setTitle("AGREGAR IGV");
@@ -46,27 +49,32 @@ public class Inter_Agre_Igv extends javax.swing.JInternalFrame {
 
         jButton1.setText("GRABAR");
 
+        txt_descripcion_igv.setColumns(20);
+        txt_descripcion_igv.setRows(5);
+        jScrollPane1.setViewportView(txt_descripcion_igv);
+
+        jLabel4.setText("Año");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(50, 50, 50)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel2)
-                            .addComponent(jLabel3))
-                        .addGap(48, 48, 48)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(txt_codigo_igv)
-                            .addComponent(txt_monto_igv)
-                            .addComponent(txt_descripcion_igv, javax.swing.GroupLayout.DEFAULT_SIZE, 171, Short.MAX_VALUE)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(151, 151, 151)
-                        .addComponent(jButton1)))
-                .addContainerGap(57, Short.MAX_VALUE))
+                .addGap(50, 50, 50)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel4)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jLabel1)
+                        .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING)))
+                .addGap(48, 48, 48)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jButton1)
+                    .addComponent(txt_monto_igv)
+                    .addComponent(txt_anio_igv)
+                    .addComponent(jScrollPane1)
+                    .addComponent(txt_codigo_igv))
+                .addContainerGap(62, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -76,16 +84,20 @@ public class Inter_Agre_Igv extends javax.swing.JInternalFrame {
                     .addComponent(jLabel1)
                     .addComponent(txt_codigo_igv, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel2)
-                    .addComponent(txt_monto_igv, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txt_monto_igv, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel3)
-                    .addComponent(txt_descripcion_igv, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 71, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 14, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(txt_anio_igv, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(13, 13, 13)
                 .addComponent(jButton1)
-                .addGap(63, 63, 63))
+                .addContainerGap())
         );
 
         pack();
@@ -97,8 +109,11 @@ public class Inter_Agre_Igv extends javax.swing.JInternalFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField txt_anio_igv;
     private javax.swing.JTextField txt_codigo_igv;
-    private javax.swing.JTextField txt_descripcion_igv;
+    private javax.swing.JTextArea txt_descripcion_igv;
     private javax.swing.JTextField txt_monto_igv;
     // End of variables declaration//GEN-END:variables
 }
